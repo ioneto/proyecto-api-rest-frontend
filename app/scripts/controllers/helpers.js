41cab19e5cd54da98e54cf3da9c5c9a28e8095c1
Ignacio Oneto <ignacio.oneto@alumnos.usm.cl>
2016-10-24 12:47:36 -0300

    Se termino el requerimiento de crear asignatura y asignar asignatura a un usuario

--- app/scripts/controllers/helpers.js
+++ app/scripts/controllers/helpers.js
@@ -1,90 +1,5 @@
 angular.module('proyectoApiRestFrontendApp')
     .factory('alert', function($uibModal) {
-        function show(modalUrl) {
-            return $uibModal.open({
-                templateUrl: modalUrl,
-                controller: function($uibModalInstance ,$scope, $resource){
-                    $scope.closeModal = function(){
-                        $uibModalInstance.dismiss('cancel');
-                    }
-
-                    $scope.submitModal = function(){
-                        var Review = $resource('http://localhost:3000/reviews/:reviewId');
-
-                    }
-                },
-            });
-        }
-
-        function showUsers(modalUrl,hola) {
-            return $uibModal.open({
-                templateUrl: modalUrl,
-                controller: function($uibModalInstance ,$scope, $resource){
-                    $scope.closeModal = function(){
-                        $uibModalInstance.dismiss('cancel');
-                    }
-
-                    var Users = $resource('http://localhost:3000/users/:userId', {userId:'@id'}); 
-                    $scope.userOne= Users.get({userId:1});
-                    $scope.users= Users.query();
-                    $scope.hola=hola;
-
-                    $scope.modalShowUser = function(){
-                        showUser('modalShowUser.html');
-                    };
-
-
-                    $scope.submitModal = function(){
-
-                    }
-                },
-            });
-        }
-
-        function showUser(modalUrl,user,users) {
-            return $uibModal.open({
-                templateUrl: modalUrl,
-                controller: function($uibModalInstance ,$scope, $resource){
-                    $scope.closeModal = function(){
-                        $uibModalInstance.dismiss('cancel');
-                    }
-
-                    $scope.users=users;
-                    $scope.user_id=user;
-
-                    $scope.findUser = function(){
-                        var Users = $resource('http://localhost:3000/users/:userId', {userId:'@id'});
-                        var users= Users.get({userId: $scope.user_id});
-                        showUserFind('modalShowUser.html',$scope.user_id, users);
-                    };
-
-                    /*$scope.findUser = function(){
-                        var Users = $resource('http://localhost:3000/users/:userId', {userId:'@id'});
-                        $scope.users= Users.get({userId: user_id});
-                        showUserFind('modalShowUser.html',user_id,users)
-                    }*/
-
-                   
-                },
-            });
-        }
-
-        function showUserFind(modalUrl,user,users) {
-            return $uibModal.open({
-                templateUrl: modalUrl,
-                controller: function($uibModalInstance ,$scope, $resource){
-                    $scope.closeModal = function(){
-                        $uibModalInstance.dismiss('cancel');
-                    }
-
-                    //var Users = $resource('http://localhost:3000/users/:userId', {userId:'@id'});
-                        //$scope.users= Users.get({userId: user_id});
-
-                    $scope.users=users;//Users.get({userId: user});
-                    $scope.user_id=user;                   
-                },
-            });
-        }
 
         function newReview(modalUrl,users,subjects,Review) {
             return $uibModal.open({
@@ -136,15 +51,49 @@ angular.module('proyectoApiRestFrontendApp')
             });
         }
 
-        function registerSubject(modalUrl,users,us) {
+        function registerUserSubject(modalUrl) {
             return $uibModal.open({
                 templateUrl: modalUrl,
-                controller: function($uibModalInstance ,$scope,$resource){
+                controller: function($uibModalInstance, $scope, $resource){
+
+                    var User = $resource('http://localhost:3000/users/:id');
+                    var Subjects = $resource('http://localhost:3000/subjects');
+                    var UserSubjects = $resource('http://localhost:3000/users/:id/user-subjects');
+
+                    $scope.users = User.get({id: 1});
+                    $scope.subjects = Subjects.query();
+                    $scope.subjects.$promise.then(function(){
+                        $scope.userSubjects = UserSubjects.query({id: 1});
+                        $scope.userSubjects.$promise.then(function() {
+                            for (var i = 0; i < $scope.userSubjects.length; i++) {
+                                for(var j = 0; j < $scope.subjects.length; j++){
+                                    if($scope.userSubjects[i].subject_id == $scope.subjects[j].id){
+                                        $scope.subjects.splice(j,1);
+                                    }
+                                }
+                            }
+                        });
+                    });
+                    $scope.newUserSubject = {};
+
+                    $scope.modalNewSubject = function(){
+                        newSubject('modalNewSubject.html',$scope.subjects)
+                    };
+
+                    $scope.saveUserSubject = function(){
+                        $scope.newUserSubject.user_id = 1;
+                        var UserSubject = $resource('http://localhost:3000/users/:idUser/subjects/:idSubject', {idUser: $scope.newUserSubject.user_id, idSubject: $scope.newUserSubject.subject_id});
+                        var newUserSubject = UserSubject.save($scope.newUserSubject);
+                        newUserSubject.$promise.then(function(){
+                            $uibModalInstance.dismiss('cancel');
+                        });
+                    }
+
                     $scope.closeModal = function(){
                         $uibModalInstance.dismiss('cancel');
                     }
 
-                    $scope.users=users;
+                    /*$scope.users=users;
                     $scope.subject={};
                     $scope.us=us;
                     //$scope.subjects=[];
@@ -181,14 +130,30 @@ angular.module('proyectoApiRestFrontendApp')
             });
         }
 
+        function newSubject(modalUrl,subjects) {
+            return $uibModal.open({
+                templateUrl: modalUrl,
+                controller: function($uibModalInstance, $scope, $resource){
+                    $scope.closeModal = function(){
+                        $uibModalInstance.dismiss('cancel');
+                    }
 
+                    $scope.saveSubject = function(){
+                        var Subject = $resource('http://localhost:3000/subjects/:id', {id: '@id'});
+                        var newSubject = Subject.save($scope.newSubject);
+                        newSubject.$promise.then(function(){
+                            subjects.push(newSubject);
+                            $uibModalInstance.dismiss('cancel');
+                        });
+                    }
+                },
+            });
+        }
 
         return {
-            show: show,
-            showUsers: showUsers,
-            showUser: showUser,
             newReview: newReview,
             showReview: showReview,
-            registerSubject: registerSubject,
+            registerUserSubject: registerUserSubject,
+            newSubject: newSubject
         };
     });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,71 +9,16 @@
  */
 angular.module('proyectoApiRestFrontendApp')
     .controller('MainCtrl', function($scope,$resource,moment, alert, calendarConfig) {
-        $scope.modalRegisterSubjectEvent = function(){
-            alert.registerSubject('modalRegisterSubject.html',$scope.users,$scope.subjects);
-        };
-
-        $scope.modalShowUsers = function(){
-            alert.showUsers('modalShowUsers.html',$scope.hola);
-        };
-
-        $scope.modalShowUser = function(){
-            alert.showUser('modalfindUser.html',$scope.user_id,{});
+        $scope.modalRegisterUserSubject = function(){
+            alert.registerUserSubject('modalRegisterUserSubject.html');
         };
 
         $scope.modalNewReview = function(){
             alert.newReview('modalNewReview.html',$scope.users,$scope.subjects);
         };
 
-        var Users = $resource('http://localhost:3000/users/:userId/', {userId:'@id'});
-        var Subjects = $resource('http://localhost:3000/users/:userId/subjects/:userSubjectId', 
-            {userId:1,userSubjectId:'@id'});
-        //var UserSubjects = $resource('http://localhost:3000/users/:userId/subjects/:userSubjectId', 
-            //{userId:1,userSubjectId:'@id'});
-        $scope.users= Users.get({userId: 1});
-        $scope.subjects= Subjects.query();
-        //$scope.us= Subjects.query();
-        //$scope.Subjects=Subjects;
-
-
-
-        $scope.hola=1;
-        $scope.user_id=0;
-
-
-
-        var vm = $scope;
-        var actions = [{
-            label: '<i class=\'glyphicon glyphicon-pencil\'></i>',
-            onClick: function(args) {
-                alert.show('Edited', args.calendarEvent);
-            }
-        /*}, {
-            label: '<i class=\'glyphicon glyphicon-remove\'></i>',
-            onClick: function(args) {
-                alert.show('Deleted', args.calendarEvent);
-            }*/
-        }];
-        vm.events = [
-            {
-                title: 'Draggable event',
-                color: calendarConfig.colorTypes.warning,
-                startsAt: moment().startOf('month').toDate(),
-                draggable: true,
-                /*resizable: true,*/
-                actions: actions
-            },
-            {
-                title: 'Non-draggable event',
-                color: calendarConfig.colorTypes.info,
-                startsAt: moment().startOf('month').toDate(),
-                draggable: false,
-                /*resizable: true,*/
-                actions: actions
-            }
-        ];
-        vm.calendarView = 'year';
-        vm.viewDate = moment().startOf('month').toDate();
-        vm.cellIsOpen = true;
+        $scope.calendarView = 'year';
+        $scope.viewDate = moment().startOf('month').toDate();
+        $scope.cellIsOpen = true;
 
     });
